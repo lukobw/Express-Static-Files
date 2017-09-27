0346d0e10a2d2239a4ec7f578c76b2c336d50ef8
Lucas <viper20@poczta.fm>
2017-09-27 18:39:00 +0200

    fix

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+
 app.use(express.static('assets'));
 
 app.get('/', (req, res) => {
@@ -11,13 +12,11 @@ app.get('/userform', (req, res) => {
         first_name: req.query.first_name,
         last_name: req.query.last_name
     };
-    res.end(JSON.stringify(reponse));
+    res.end(JSON.stringify(response));
 });
 
-var server = app.listen(3001, () => {
-    var host = server.address().address;
-    var port = server.address().post;
+var server = app.listen(3000, () => {
+    var port = server.address().port;
 
-    console.log(`Application served on http://${host}:${port}`);
+    console.log(`Application served on http://localhost:${port}`);
 });
-
